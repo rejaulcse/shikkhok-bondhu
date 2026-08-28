04754273653acc656bf747a432685edbcce47ec1
Rejaul Islam <rejaulbrur321@gmail.com>
2026-08-28 11:32:10 +0600

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const APP_CONFIG = {
       title: "বদলি ব্যবস্থাপনা",
       children: [
         { title: "কলেজ বদলি", url: "https://ngttvacant.vercel.app" },
-        { title: "মাদ্রাসা বদলি", url: "https://shorturl.at/qcs0z" },
+        { title: "মাদ্রাসা বদলি", url: "https://ngttdm.vercel.app/" },
       ],
     },
     {
